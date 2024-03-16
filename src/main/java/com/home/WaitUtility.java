1a287941ff23153b64adff127a57dbbdefb867c8
Dibya Saha <dibya.ahas1996@gmail.com>
2024-03-16 21:24:43 +0530

    Added Explicit wait utility

---
+++ src/main/java/com/home/WaitUtility.java
@@ -0,0 +1,18 @@
+package com.home;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtility {
+	
+	public WebElement waitForElement(WebDriver driver, By by) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+	}
+
+}
