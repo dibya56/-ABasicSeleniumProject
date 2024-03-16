f05bdf20309b67d8cefaa4cdbe720921c945575d
Dibya Saha <dibya.ahas1996@gmail.com>
2024-03-16 17:57:08 +0530

    Writing First Test

---
+++ src/test/java/com/home/FirstTest.java
@@ -0,0 +1,40 @@
+package com.home;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+public class FirstTest {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void startUp() throws IOException {
+		Properties prop = PropertyManager.readPropertiesFile("Resources.properties");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get(prop.getProperty("url"));
+	}
+	
+	@Test
+	public void test1() throws InterruptedException {
+		Thread.sleep(3000);
+		String header = driver.findElement(By.xpath("//h1[@class='heading']")).getText();
+		Assert.assertEquals("Header Mismatch", "Welcome to the-internet", header);
+	}
+	
+	@AfterTest
+	public void cleanUp() {
+		driver.quit();
+	}
+
+}
